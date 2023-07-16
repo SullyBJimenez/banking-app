e407a1006ace037783b4179fa46f602b70e4c1a6
sullybjimenez <sullybjimenez@gmail.com>
2023-07-16 15:40:21 -0400

    removed console log

--- src/Components/login.js
+++ src/Components/login.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import { Card } from './context.js';
 
 export function Login(){
-    console.log('hello')
+    
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [show, setShow] = React.useState(true);
