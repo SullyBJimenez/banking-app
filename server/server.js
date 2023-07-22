54a9f2995bea4d12c2f9c2fc7af2aa0a91c42503
sullybjimenez <sullybjimenez@gmail.com>
2023-07-21 21:33:46 -0400

    updated yml

--- server/server.js
+++ server/server.js
@@ -2,10 +2,42 @@ import  express  from "express";
 import mongoose from "mongoose";
 import profileRouter from "./routes/profiles-routes.js";
 import cors from "cors";
+import 'dotenv/config';
+// import {
+//     SecretsManagerClient,
+//     GetSecretValueCommand,
+//   } from "@aws-sdk/client-secrets-manager";
+  import { util } from '@aws-appsync'
 
+  export function request(ctx) {
+      return {
+          method: 'POST',
+          version: '2018-05-29',
+          resourcePath: '/',
+          params: {
+              headers: {
+                  'content-type': 'application/x-amz-json-1.1',
+                  'x-amz-target': 'secretsmanager.GetSecretValue',
+              },
+              body: {
+                  SecretId: 'APPSYNC_MONGO_API_KEY',
+              },
+          },
+      }
+  }
+  
+  export function response(ctx) {
+      //"{Name: 'APPSYNC_MONGO_API_KEY', SecretString: 'abc123'}"
+      const result = JSON.parse(ctx.result.body).SecretString
+      console.log(result)
+      return result
+  }
+
+  response();
+  
 const app = express();
-const port = 2020;
-const url = 'mongodb+srv://sullybjimenez:Et1qrPfzonmTYRW8@cluster0.ff08cbv.mongodb.net/';
+const port = process.env.PORT || 2020;
+const url = process.env.MONGO_URL;
 
 app.use(express.json());
 app.use(cors());
@@ -17,4 +49,24 @@ app.use(profileRouter);
 app.listen(port, () => {
     console.log('Listening on port: ' + port);
 });
-
+  
+//   const secret_name = "APPSYNC_MONGO_API_KEY";
+  
+//   const client = new SecretsManagerClient({
+//     region: "us-east-2",
+//   });
+  
+//   let response;
+  
+//   try {
+//     response = await client.send(
+//       new GetSecretValueCommand({
+//         SecretId: secret_name,
+//         VersionStage: "AWSCURRENT", 
+//       })
+//     );
+//   } catch (error) {
+//     throw error;
+//   }
+  
+//   export const secret = response.SecretString;
