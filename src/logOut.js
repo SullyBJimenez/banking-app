df6134851dd525609ef3e7f34e64c9ce0a768fb6
sullybjimenez <sullybjimenez@gmail.com>
2023-07-23 00:01:08 -0400

    changed logout to a component and added function within

--- src/logOut.js
+++ src/logOut.js
@@ -1,6 +1,14 @@
+import { useNavigate } from "react-router-dom"
 
-export const logOut = () => {
-    window.localStorage.setItem("loggedIn", false)
-    window.localStorage.clear();
-    window.location.href = "/home/"
+export const LogOut = () => {
+    const navigate = useNavigate();
+
+    const logout = () => {
+        window.localStorage.setItem("loggedIn", false)
+        window.localStorage.clear();
+        navigate("/home/")
+    }
+
+    logout()
+   
 }
